3a497413309f6be6637d7b6ad2f37b5e4323f042
wanghui <wanghui4@musnhealth.com>
2024-06-15 20:32:16 +0800

    第一次提交

---
+++ src/main/java/SQLGen.java
@@ -0,0 +1,10 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class SQLGen {
+    public static void main(String[] args) {
+
+    }
+}
